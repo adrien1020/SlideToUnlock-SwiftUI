eaaa065b153dceb8021ae8960f1a3509afca89c6
Adrien <adrien.surugue@gmail.com>
2024-09-02 22:01:25 +0200

    Initial Commit

---
+++ SlideToUnlock/SlideToUnlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideToUnlockApp.swift
+//  SlideToUnlock
+//
+//  Created by Adrien Surugue on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SlideToUnlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
